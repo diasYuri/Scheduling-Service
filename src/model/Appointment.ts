a3459f3c994f3ef72bab0fb9ad7763286c891443
Yuri Dias <yuri.dazevedo@gmail.com>
2021-04-12 11:59:17 -0300

    foreign-key migration

--- src/model/Appointment.ts
+++ src/model/Appointment.ts
@@ -1,24 +1,28 @@
-import { Entity, Column, PrimaryGeneratedColumn, UpdateDateColumn, CreateDateColumn } from 'typeorm'
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  CreateDateColumn,
+} from "typeorm";
 // tslint:disable: variable-name
 
-@Entity('appointments')
+@Entity("appointments")
 class Appointments {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
 
-  @PrimaryGeneratedColumn('uuid')
-  id: string
+  @Column()
+  provider_id: string;
 
-  @Column('varchar')
-  provider_id: string
-
-  @Column('time with time zone')
-  date: Date
+  @Column("time with time zone")
+  date: Date;
 
   @CreateDateColumn()
-  created_at: Date
+  created_at: Date;
 
   @UpdateDateColumn()
-  updated_at: Date
-
+  updated_at: Date;
 }
 
-export default Appointments
+export default Appointments;
