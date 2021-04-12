0ac6db0b1a2b4a3e4743afe631854ed34b51e74d
Yuri Dias <yuri.dazevedo@gmail.com>
2021-04-12 10:45:26 -0300

    first commit

---
+++ src/database/migrations/1618230635683-CreateAppointments.ts
@@ -0,0 +1,41 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateAppointments1618230635683 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: "appointments",
+            columns: [
+                {
+                    name: "id",
+                    type: "uuid",
+                    isPrimary: true,
+                    generationStrategy: 'uuid'
+                },
+                {
+                    name: "provider",
+                    type: "varchar",
+                },
+                {
+                    name: "date",
+                    type: "timestamp with time zone",
+                },
+                {
+                    name: "created_at",
+                    type: "timestamp",
+                    default: "now()"
+                },
+                {
+                    name: "updated_at",
+                    type: "timestamp",
+                    default: "now()"
+                }
+            ]
+        }), true)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('appointments')
+    }
+
+}
---
+++ src/model/Appointment.ts
@@ -0,0 +1,24 @@
+import { Entity, Column, PrimaryGeneratedColumn, UpdateDateColumn, CreateDateColumn } from 'typeorm'
+// tslint:disable: variable-name
+
+@Entity('appointments')
+class Appointments {
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column('varchar')
+  provider_id: string
+
+  @Column('time with time zone')
+  date: Date
+
+  @CreateDateColumn()
+  created_at: Date
+
+  @UpdateDateColumn()
+  updated_at: Date
+
+}
+
+export default Appointments
