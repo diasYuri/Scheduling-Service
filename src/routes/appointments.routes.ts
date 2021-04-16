05a57a3610167bbd134f5ca583975124c111f683
Yuri Dias <yuri.dazevedo@gmail.com>
2021-04-16 10:56:38 -0300

    feat: use authentication middleware to appointmentRoutes

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,11 +1,14 @@
 import { parseISO } from 'date-fns';
 import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
+import ensureAuthenticated from '../middleware/ensureAuthenticated';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
+
+appointmentsRouter.use(ensureAuthenticated)
 
 appointmentsRouter.get('/', async (request, response) => {
   const appointmentsRepository = getCustomRepository(AppointmentsRepository)
