50abb99ac61fe5108af37a345bd1888dd7deff31
Yuri Dias <yuri.dazevedo@gmail.com>
2021-04-16 11:33:46 -0300

    feat: create new migration to add avatarField

---
+++ src/database/migrations/1618583547303-AddAvatarFieldToUsers.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AddAvatarFieldToUsers1618583547303 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
