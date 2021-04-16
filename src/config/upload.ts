6d41e6616f4e66c42de366a5739652eee4f349b3
Yuri Dias <yuri.dazevedo@gmail.com>
2021-04-16 16:31:23 -0300

    update: upload config

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,11 +2,14 @@ import path from 'path'
 import crypto from 'crypto'
 import multer from 'multer'
 
-export default {
+const tempFolder = path.resolve(__dirname, '..', '..', 'temp')
 
-  storeage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'temp'), filename(req, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('HEX')
+export default {
+  diretory: tempFolder,
+
+  storage: multer.diskStorage({
+    destination: tempFolder, filename(req, file, callback) {
+      const fileHash = crypto.randomBytes(10).toString('hex')
       const fileName = `${fileHash}-${file.originalname}`;
 
       return callback(null, fileName)
