1348b3d262ffd1fe02ce1944afae094febcd423a
Yuri Dias <yuri.dazevedo@gmail.com>
2021-04-23 17:35:34 -0300

    feat: static file route

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import express from "express";
-import { Request, Response, NextFunction } from "express";
-//import "express-async-errors";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
+import uploadConfig from "./config/upload";
 
 import "./database";
 import { AppError } from "./error/error";
@@ -11,6 +11,7 @@ const app = express();
 const port = 3030;
 
 app.use(express.json());
+app.use("/files", express.static(uploadConfig.diretory));
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
